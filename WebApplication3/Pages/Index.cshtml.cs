3c3f3f065d7b22a69a0ad41db2773e4e108f892c
Temessadouno <temessadouno46@gmail.com>
2026-01-04 02:47:57 +0100

    Remove timer logic from IndexModel

--- WebApplication3/Pages/Index.cshtml.cs
+++ WebApplication3/Pages/Index.cshtml.cs
@@ -7,14 +7,9 @@ namespace WebApplication3.Pages;
 public class IndexModel : PageModel
 
 {
-    public long timer { get; set; }
+   
     public void OnGet()
     {
-        var stopwatch = Stopwatch.StartNew(); // Début mesure
-        // Simuler un travail
-        //System.Threading.Thread.Sleep(100);
-        stopwatch.Stop(); // Fin mesure
-        timer = stopwatch.ElapsedMilliseconds;
-
+       
     }
 }
